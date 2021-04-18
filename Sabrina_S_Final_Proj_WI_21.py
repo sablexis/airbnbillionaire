934ad2b12b80d92ada16a4230ffdfc2c7d2201a5
Teen Witch Dev <sabrina.a.sandy@gmail.com>
2021-04-18 13:57:38 -0300

    :arrow_up: added image part of matplot lib

--- Sabrina_S_Final_Proj_WI_21.py
+++ Sabrina_S_Final_Proj_WI_21.py
@@ -2,5 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import matplotlib.image as mpimg
 nyc_bnb = pd.read_csv("AB_NYC_2019.csv")
+print(nyc_bnb)
